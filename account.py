99c81fd7c01cb65cf025e1d13a51aca2e6bbf903
farzana583 <farzanaisack@gmail.com>
2020-11-21 23:46:28 +0300

    add account.py

--- account.py
+++ account.py
@@ -4,11 +4,11 @@ class Account:
     """
     account_list = []
 
-    # def __init__(self, account_name,user_name,password,email):
-    #     self.account_name = account_name
-    #     self.user_name = user_name
-    #     self.password = password
-    #     self.email = email
+    def __init__(self, account_name,user_name,password,email):
+        self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
 
     def save_account(self):
 
